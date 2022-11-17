5bd8f9eaaada144966bf0816630a434898e5c2d1
abraham <apolichenco@gmail.com>
2022-11-16 22:23:06 -0500

    commit tree e22473eeb3514d139b16f76cb9f3c809a56760ed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
+require 'pry'
+
 class Product < ActiveRecord::Base
     belongs_to :category
+
+    def sell_it_for
+        gain = self.price * 0.3
+        self.price + gain
+        binding.pry
+    end
 end
